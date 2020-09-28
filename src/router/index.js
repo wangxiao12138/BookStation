62c7abfb1782a18f9aba05d073bf6bbf7d661a54
wangxiao <wangxiao@egoonet.com>
2020-09-28 19:00:44 +0800

    幻灯片跳转，详细页面路由跳转

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import IndexPage from "@/pages/index"
+import DetailPage from '@/pages/detail'
+import DetailAnaPage from '@/pages/detail/analysis'
+import DetailCouPage from '@/pages/detail/count'
+import DetailForPage from '@/pages/detail/forecast'
+import DetailPubPage from '@/pages/detail/publish'
+
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +15,29 @@ export default new Router({
       path: '/',
       name: 'IndexPage',
       component: IndexPage
+    },
+    {
+      path: '/detail',
+      component: DetailPage,
+      redirect: '/detail/analysis',
+      children: [
+        {
+          path: 'analysis',
+          component: DetailAnaPage
+        },
+        {
+          path: 'count',
+          component: DetailCouPage
+        },
+        {
+          path: 'forecast',
+          component: DetailForPage
+        },
+        {
+          path: 'publish',
+          component: DetailPubPage
+        }
+      ]
     }
   ]
 })
